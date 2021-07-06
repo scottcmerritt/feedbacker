5012928b661e7e43955c90f1d5bb09bbd6108508
Scott Merritt <scott.merritt@gmail.com>
2021-07-06 06:01:07 -0700

    first commit

--- app/helpers/feedbacker/application_helper.rb
+++ app/helpers/feedbacker/application_helper.rb
@@ -5,10 +5,9 @@ module Feedbacker
     def feedbacker_draw target:nil, data: nil 
       "Hello world new version"
 
+      path_prepped = (target.class.name.downcase+"s_path").to_sym
 
-      my_path = (target.class.name.downcase+"s_path").to_sym
-
-      render(partial:"shared/hello", locals: {target: target,path_to_resource:my_path, data:data}) # + render(partial:"shared/hello2")
+      render(partial:"shared/hello", locals: {target: target, path_to_resource:path_prepped, data:data}) # + render(partial:"shared/hello2")
     end
 
 
