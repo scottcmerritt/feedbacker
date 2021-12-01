abc4711333bff0488190970e856ae762ac14d362
Scott Merritt <scott.merritt@gmail.com>
2021-11-30 23:54:27 -0800

    visits update

--- lib/feedbacker/concerns/feedbacker_admin_controller.rb
+++ lib/feedbacker/concerns/feedbacker_admin_controller.rb
@@ -234,6 +234,7 @@ def cleanup
 
 
     def visits
+      @users = User.all.sort_by{|user| (user.last_view ? user.last_view : 100.days.ago)}.reverse
 
       wh_sql = params[:users] ? "user_id > 0" : "(user_id is NULL OR user_id > 0) OR ip_address is null" 
       @visitors = Impression.select("ip_address,COUNT(id) as view_count,MAX(user_id) as user_id,MAX(created_at) as last_visit")
